0ca358410208efd07e5db190cbb32b6e548c3962
Montinator <florian.mortier4599@gmail.com>
2018-04-03 15:38:41 +0200

    Unity Projekt

---
+++ P_SEMI_VERSUCH/Assets/Scripts/CamaraMovementPlayer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraMovementPlayer : MonoBehaviour {
+
+	public float speedMove;
+	public float speedRot;
+	private Vector3 rot;
+	public Rigidbody rb;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 pos = transform.position;
+
+
+		if (Input.GetKey ("left")) {
+			pos.x += -speedMove * Time.deltaTime;
+		}
+		if (Input.GetKey ("right")) {
+			pos.x += speedMove * Time.deltaTime;
+		}
+
+		if (Input.GetKey ("up")) {
+			if (pos.y < 400) {
+				pos.y += speedMove * Time.deltaTime;
+				speedMove = 10f;
+			}
+		}
+
+		if (Input.GetKey ("down")) {
+			pos.y += -speedMove * Time.deltaTime;
+		}
+
+		if (Input.GetKey ("w")) {
+			pos.z += speedMove * Time.deltaTime;
+		}
+
+		if (Input.GetKey ("s")) {
+			pos.z += -speedMove * Time.deltaTime;
+		}
+
+		if (Input.GetKey ("a")) {
+			rot = new Vector3 (0, speedRot * Time.deltaTime, 0);
+		}else if (Input.GetKey ("d")) {
+			rot = new Vector3 (0, -speedRot * Time.deltaTime, 0);
+		} else {
+			rot = new Vector3 (0, 0, 0);
+		}
+
+
+
+
+
+		transform.position = pos;
+		transform.eulerAngles = transform.eulerAngles - rot;
+
+	}
+	void OnTriggerEnter(Collider col){
+		speedMove = 0f;
+	}
+		
+}
---
+++ P_SEMI_VERSUCH/Assets/Scripts/DayNight.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class DayNight : MonoBehaviour {
+
+
+	public float time;
+	public TimeSpan currantTime;
+	public Transform sunTransf;
+	public Light sun;
+	public Text timeText;
+
+
+	public float intensity;
+	public Color foggyday = Color.grey;
+	public Color foggyNight = Color.black;
+
+	public int speed;
+	// Update is called once per frame
+	void Update () {
+		ChangeTime ();
+
+	}
+
+	public void ChangeTime(){
+		time += Time.deltaTime * speed;
+		if (time > 86400) {
+			time = 0;
+		}
+
+		currantTime = TimeSpan.FromSeconds (time);
+		string[] timet = currantTime.ToString ().Split (":" [0]);
+		timeText.text = timet [0] + ":" + timet [1];
+
+		sunTransf.rotation = Quaternion.Euler (new Vector3 ((time - 21600) / 86400 * 360, 0));
+
+		if (time > 79000 || time < 10000) {
+			sun.intensity = 0.2f;
+		} else if (time > 67000) {
+			sun.intensity = 1.3f;
+		} else {
+			sun.intensity = 1;
+		}
+
+			
+
+	/*	if (time > 43200) {
+			intensity = 1 - (43200 - time) / 43200;
+		} else {
+			intensity = 1 - ((43200 - time) / 43200 * -1);
+		}
+
+		RenderSettings.fogColor = Color.Lerp (foggyday, foggyNight, intensity * intensity);
+
+		sun.intensity = intensity;*/
+	}
+
+
+}
