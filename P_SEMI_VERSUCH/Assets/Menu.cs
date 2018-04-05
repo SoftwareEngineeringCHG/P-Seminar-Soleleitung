7ee57f0edee7b43ea95d587ca4cd8903f4cf3802
Schabolon <jakob.guenthinger@gmx.de>
2018-04-05 02:40:09 +0200

    Menu-Screen implementiert

---
+++ P_SEMI_VERSUCH/Assets/Menu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public static bool menuVisible = false;
+
+    public GameObject menuUI;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (menuVisible)
+            {
+                closeMenu();
+            } else
+            {
+                openMenu();
+            }
+        }
+	}
+
+    void closeMenu()
+    {
+        menuUI.SetActive(false);
+        Time.timeScale = 1f;
+        menuVisible = false;
+    }
+
+    void openMenu()
+    {
+        menuUI.SetActive(true);
+        Time.timeScale = 0f;
+        menuVisible = true;
+    }
+
+    public void rundflug()
+    {
+
+    }
+
+    public void brunnhausBesichtigen()
+    {
+
+    }
+
+    public void freierFlug()
+    {
+
+    }
+}
